1f3eaf80264ee0e6e0babd8f8288ed2753da4a41
Jarl Gullberg <jarl.gullberg@gmail.com>
2016-04-08 19:54:07 +0200

    Another cleanup pass.

--- Everlook/Program.cs
+++ Everlook/Program.cs
@@ -19,14 +19,13 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-using System;
 using Gtk;
 
 namespace Everlook
 {
 	class MainClass
 	{
-		public static void Main(string[] args)
+		public static void Main()
 		{
 			Application.Init();
 			MainWindow win = MainWindow.Create();
