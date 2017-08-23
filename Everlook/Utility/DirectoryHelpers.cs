30eea7587300676db55eab9690b2da2460145f8c
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-08-23 13:09:35 +0200

    Update copyright year in file headers.

--- Everlook/Utility/DirectoryHelpers.cs
+++ Everlook/Utility/DirectoryHelpers.cs
@@ -4,7 +4,7 @@
 //  Author:
 //       Jarl Gullberg <jarl.gullberg@gmail.com>
 //
-//  Copyright (c) 2016 Jarl Gullberg
+//  Copyright (c) 2017 Jarl Gullberg
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
