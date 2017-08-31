1806d966e3bd17529be2f5da4a22d2e5269eb522
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-08-31 23:31:44 +0200

    Add include guards to combiners.

--- Everlook/Content/Shaders/Components/Combiners/Combiners.glsl
+++ Everlook/Content/Shaders/Components/Combiners/Combiners.glsl
@@ -1,3 +1,28 @@
+//
+//  Combiners.glsl
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef Combiners_I
+#define Combiners_I
+
 vec4 CombinersAdd(vec4 colour, vec4 texture)
 {
 	return colour + texture;
@@ -178,3 +203,5 @@ vec4 CombinersOpaqueOpaque(vec4 colour, vec4 texture0, vec4 texture1)
 {
 	return CombinersOpaque(CombinersOpaque(colour, texture0), texture1);
 }
+
+#endif
