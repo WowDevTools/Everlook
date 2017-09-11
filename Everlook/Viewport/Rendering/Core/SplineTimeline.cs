a666ff76202757765a090c402a953bdd5b737450
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-09-11 22:09:28 +0200

    Use documentation inheritance.

--- Everlook/Viewport/Rendering/Core/SplineTimeline.cs
+++ Everlook/Viewport/Rendering/Core/SplineTimeline.cs
@@ -71,14 +71,7 @@ namespace Everlook.Viewport.Rendering.Core
 			}
 		}
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Timeline{T}"/> class. This constructor overrides the duration
-		/// of the timeline with the given value.
-		/// </summary>
-		/// <param name="interpolation">The interpolation type of the timeline.</param>
-		/// <param name="timestamps">The timestamps in the timeline.</param>
-		/// <param name="values">The values at the timestamps in the timeline.</param>
-		/// <param name="duration">The duration of the timeline.</param>
+		/// <inheritdoc />
 		public SplineTimeline(InterpolationType interpolation, IReadOnlyCollection<uint> timestamps, IReadOnlyCollection<SplineKey<T>> values, float duration)
 			: base(interpolation, timestamps, values)
 		{
