3fe81adea49d9e1a0e6c51eecfaee5903815d392
Jarl Gullberg <jarl.gullberg@gmail.com>
2020-04-04 14:36:00 +0200

    Add an initial Assimp export class.

---
+++ Everlook/Export/Model/AssimpConverter.cs
@@ -0,0 +1,128 @@
+//
+//  AssimpConverter.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Linq;
+using Assimp;
+using Warcraft.MDX;
+
+namespace Everlook.Export.Model
+{
+	/// <summary>
+	/// Converts models to Assimp representations.
+	/// </summary>
+	public static class AssimpConverter
+	{
+		/// <summary>
+		/// Converts the given MDX model into an Assimp representation.
+		/// </summary>
+		/// <param name="model">The model.</param>
+		/// <returns>The Assimp representation.</returns>
+		public static Scene FromMDX(MDX model)
+		{
+			var scene = new Scene();
+
+			var rootNode = new Node();
+			scene.RootNode = rootNode;
+
+			var modelNode = new Node(model.Name, rootNode);
+			rootNode.Children.Add(modelNode);
+
+			var defaultMaterial = new Material();
+			scene.Materials.Add(defaultMaterial);
+
+			foreach (var skin in model.Skins)
+			{
+				var skinNode = new Node($"LOD_{model.Skins.ToList().IndexOf(skin)}", modelNode);
+				modelNode.Children.Add(skinNode);
+
+				var skinVerts = skin.VertexIndices.Select(i => model.Vertices[i]).ToArray();
+
+				foreach (var section in skin.Sections)
+				{
+					var mesh = new Mesh();
+					scene.Meshes.Add(mesh);
+
+					mesh.MaterialIndex = scene.Materials.IndexOf(defaultMaterial);
+
+					var modelBones = model.Bones.Skip(section.StartBoneIndex).Take(section.BoneCount);
+					foreach (var modelBone in modelBones)
+					{
+						var bone = new Bone();
+
+						// TODO: Calculate offset matrices
+						mesh.Bones.Add(bone);
+					}
+
+					var batchNode = new Node($"Section_{skin.Sections.ToList().IndexOf(section)}", skinNode);
+					skinNode.Children.Add(batchNode);
+
+					batchNode.MeshIndices.Add(scene.Meshes.IndexOf(mesh));
+
+					var skinVertexIndexes = new Span<ushort>
+					(
+						skin.VertexIndices.ToArray(),
+						section.StartVertexIndex,
+						section.VertexCount
+					).ToArray();
+
+					mesh.UVComponentCount[0] = 2;
+					mesh.UVComponentCount[1] = 2;
+
+					for (int i = 0; i < skinVertexIndexes.Length; ++i)
+					{
+						var localIndex = skinVertexIndexes[i];
+						var vertex = skinVerts[localIndex];
+
+						mesh.Vertices.Add(new Vector3D(vertex.Position.X, vertex.Position.Y, vertex.Position.Z));
+						mesh.Normals.Add(new Vector3D(vertex.Normal.X, vertex.Normal.Y, vertex.Normal.Z));
+
+						mesh.TextureCoordinateChannels[0].Add(new Vector3D(vertex.UV1.X, vertex.UV1.Y, 0.0f));
+						mesh.TextureCoordinateChannels[1].Add(new Vector3D(vertex.UV2.X, vertex.UV2.Y, 0.0f));
+
+						if (mesh.HasBones)
+						{
+							for (int boneAttributeIndex = 0; boneAttributeIndex < 4; ++boneAttributeIndex)
+							{
+								var bone = mesh.Bones[vertex.BoneIndices[boneAttributeIndex]];
+
+								var weight = vertex.BoneWeights[boneAttributeIndex];
+								bone.VertexWeights.Add(new VertexWeight(i, weight));
+							}
+						}
+					}
+
+					var triangleIndexes = new Span<ushort>
+					(
+						skin.Triangles.ToArray(),
+						section.StartTriangleIndex,
+						section.TriangleCount
+					).ToArray();
+
+					mesh.SetIndices(triangleIndexes.Select(index => (int)index).ToArray(), 3);
+				}
+			}
+
+			return scene;
+		}
+	}
+}
